9e1e28ea7650e078286c00baebb2826c49a423fe
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-23 14:39:45 +0100

    Impostato array di post

--- js/script.js
+++ js/script.js
@@ -1,1 +1,76 @@
-console.log("ok");
+/*
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+# Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+- id del post, numero progressivo da 1 a n
+- nome autore,
+- foto autore (potrebbe mancare a qualcuno),
+- data in formato americano (mm-gg-yyyy),
+- testo del post,
+- immagine (non tutti i post devono avere una immagine),
+- numero di likes.
+*Non è necessario creare date casuali, inventatele*
+#Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+#Milestone 3
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+*/
+
+//Creo array di post
+const posts = [
+  {
+    id: 1,
+    author: "Pinco Pallino",
+    authorPic: "https://picsum.photos/200/300?",
+    date: "12-23-2022",
+    text: "ciao",
+    image: "post img",
+    numberOfLike: 3,
+  },
+  {
+    id: 1,
+    author: "Pinco Pallino",
+    authorPic: "https://picsum.photos/200/300?",
+    date: "12-23-2022",
+    text: "ciao",
+    image: "post img",
+    numberOfLike: 3,
+  },
+  {
+    id: 1,
+    author: "Pinco Pallino",
+    authorPic: "https://picsum.photos/200/300?",
+    date: "12-23-2022",
+    text: "ciao",
+    image: "post img",
+    numberOfLike: 3,
+  },
+  {
+    id: 1,
+    author: "Pinco Pallino",
+    authorPic: "https://picsum.photos/200/300?",
+    date: "12-23-2022",
+    text: "ciao",
+    image: "post img",
+    numberOfLike: 3,
+  },
+  {
+    id: 1,
+    author: "Pinco Pallino",
+    authorPic: "https://picsum.photos/200/300?",
+    date: "12-23-2022",
+    text: "ciao",
+    image: "post img",
+    numberOfLike: 3,
+  },
+  {
+    id: 1,
+    author: "Pinco Pallino",
+    authorPic: "https://picsum.photos/200/300?",
+    date: "12-23-2022",
+    text: "ciao",
+    image: "post img",
+    numberOfLike: 3,
+  },
+];
