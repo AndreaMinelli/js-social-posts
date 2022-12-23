38fe0d015028f34f5fdfc9ac38bd2d159c0e7ab7
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-23 15:16:10 +0100

    Stampati post in pagina

--- js/script.js
+++ js/script.js
@@ -24,53 +24,122 @@ const posts = [
     author: "Pinco Pallino",
     authorPic: "https://picsum.photos/200/300?",
     date: "12-23-2022",
-    text: "ciao",
-    image: "post img",
-    numberOfLike: 3,
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio\
+            minima iusto. Ad ad maiores et sint voluptate recusandae architecto.\
+            Et nihil ullam aut alias.",
+    image: "https://picsum.photos/200/300?",
+    likesCounter: 3,
   },
   {
     id: 1,
     author: "Pinco Pallino",
     authorPic: "https://picsum.photos/200/300?",
     date: "12-23-2022",
-    text: "ciao",
-    image: "post img",
-    numberOfLike: 3,
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio\
+            minima iusto. Ad ad maiores et sint voluptate recusandae architecto.\
+            Et nihil ullam aut alias.",
+    image: "https://picsum.photos/200/300?",
+    likesCounter: 3,
   },
   {
     id: 1,
     author: "Pinco Pallino",
     authorPic: "https://picsum.photos/200/300?",
     date: "12-23-2022",
-    text: "ciao",
-    image: "post img",
-    numberOfLike: 3,
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio\
+            minima iusto. Ad ad maiores et sint voluptate recusandae architecto.\
+            Et nihil ullam aut alias.",
+    image: "https://picsum.photos/200/300?",
+    likesCounter: 3,
   },
   {
     id: 1,
     author: "Pinco Pallino",
     authorPic: "https://picsum.photos/200/300?",
     date: "12-23-2022",
-    text: "ciao",
-    image: "post img",
-    numberOfLike: 3,
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio\
+            minima iusto. Ad ad maiores et sint voluptate recusandae architecto.\
+            Et nihil ullam aut alias.",
+    image: "https://picsum.photos/200/300?",
+    likesCounter: 3,
   },
   {
     id: 1,
     author: "Pinco Pallino",
     authorPic: "https://picsum.photos/200/300?",
     date: "12-23-2022",
-    text: "ciao",
-    image: "post img",
-    numberOfLike: 3,
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio\
+            minima iusto. Ad ad maiores et sint voluptate recusandae architecto.\
+            Et nihil ullam aut alias.",
+    image: "https://picsum.photos/200/300?",
+    likesCounter: 3,
   },
   {
     id: 1,
     author: "Pinco Pallino",
     authorPic: "https://picsum.photos/200/300?",
     date: "12-23-2022",
-    text: "ciao",
-    image: "post img",
-    numberOfLike: 3,
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio\
+            minima iusto. Ad ad maiores et sint voluptate recusandae architecto.\
+            Et nihil ullam aut alias.",
+    image: "https://picsum.photos/200/300?",
+    likesCounter: 3,
   },
 ];
+
+//Targhettizzo gli elementi della pagina
+const postsListElement = document.querySelector(".posts-list");
+
+let postList = "";
+
+posts.forEach((post) => {
+  const { author, authorPic, date, text, image, likesCounter } = post;
+
+  postList += `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">
+                <div class="post-meta__icon">
+                <img
+                    class="profile-pic"
+                    src="${authorPic}"
+                    alt="${author}"
+                />
+                </div>
+                <div class="post-meta__data">
+                <div class="post-meta__author">${author}</div>
+                <div class="post-meta__time">${date}</div>
+                </div>
+            </div>
+            </div>
+            <div class="post__text">${text}</div>
+            <div class="post__image">
+            <img src="${image}" alt="" />
+            </div>
+            <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                <button
+                    class="like-button js-like-button"
+                    href="#"
+                    data-postid="1"
+                >
+                    <i
+                    class="like-button__icon fas fa-thumbs-up"
+                    aria-hidden="true"
+                    ></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </button>
+                </div>
+                <div class="likes__counter">
+                Piace a
+                <b id="like-counter-1" class="js-likes-counter">${likesCounter}</b> persone
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+  return postList;
+}, "");
+
+postsListElement.innerHTML = postList;
